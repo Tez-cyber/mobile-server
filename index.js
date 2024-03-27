71b53de60676e31264c65b801886547b71cc9908
Salami Azeez <salamitopeazeez@gmail.com>
2024-03-26 19:49:13 -0700

    Linked database in index file

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import express from "express";
-import dotenv from "dotenv"
+import { config } from "./config/config.js";
+import mongoose from "mongoose";
 
-dotenv.config()
+
 const app = express()
 
 //Middlewares
 app.use(express.json())
 
+//Linking db
+const startServer = async () => {
+    try {
+        await mongoose.connect(config.mongo.url, {
+            w: "majority",
+            retryWrites: true,
+            authMechanism: "DEFAULT"
+        })
+        console.log("Database connected successfully....")
 
-const PORT = 4400
-app.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`)
-})
+        const PORT = 4400
+        app.listen(PORT, () => {
+            console.log(`Server started on port: ${PORT}`)
+        })
+    } catch (err) {
+        console.log("Database is not connected..")
+    }
+}
+
+startServer()
