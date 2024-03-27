767aee525d9fff0bca1d71fc52c4f96a0c76ad2d
Salami Azeez <salamitopeazeez@gmail.com>
2024-03-26 19:33:04 -0700

    Added mongo atlas url to connect db

---
+++ config/config.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv"
+
+dotenv.config()
+
+//Connecting to mongo atlas
+const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@mobilesystem.1zaroed.mongodb.net/`;
+
+
