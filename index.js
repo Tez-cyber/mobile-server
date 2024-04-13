e34b03cb5e876c6f179ec7d1408018587879bd9f
Salami Azeez <salamitopeazeez@gmail.com>
2024-04-12 19:28:52 -0700

    Added swagger documentation for register api in routes/auth.js

--- index.js
+++ index.js
@@ -19,15 +19,13 @@ const startServer = async () => {
     try {
         await mongoose.connect(config.mongo.url, {
             w: "majority",
-            retryWrites: true, 
+            retryWrites: true,
             authMechanism: "DEFAULT"
         })
         console.log("Database connected successfully....")
 
 
-        //routes
-        app.use("/api/auth", authRoute)
-    
+
         const options = {
             definition: {
                 openai: "3.0.0",
@@ -41,8 +39,8 @@ const startServer = async () => {
                         }
                     ]
                 },
-                
-            }, 
+                schemes: ["http", "https"]
+            },
             apis: ["./routes/*.js"]
         }
 
@@ -50,20 +48,12 @@ const startServer = async () => {
         app.use(
             "/api-docs", swaggerui.serve, swaggerui.setup(swaggerSpec)
         )
-        /**
-         * 
-         * @swagger
-         * /:
-         *  get:
-         *      summary: This api is used to check if get method is functioning
-         *      description: Check get method
-         *      responses: 
-         *          200:
-         *              descripition: To test get method 
-         * 
-         */
-        app.get("/" , (req, res) => {
-            res.send("Server is now Launched") 
+        //routes
+        app.use("/api/auth", authRoute)
+
+        
+        app.get("/", (req, res) => {
+            res.send("Server is now Launched")
         })
         // Starting app
         app.listen(PORT, () => {
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,20 @@ const router = express.Router()
 
 import authController from "../controller/authController.js"
 
+/**
+ * @swagger
+ * /register:
+ *  post:
+ *   summary: Register user
+ *   description: Allows new user to register info
+ *   tags:
+ *      - Authentication
+ *   responses:
+ *      '200':
+ *          description: User registered successfully
+ *      '400':
+ *          description: Error registering user 
+ */
 router.post("/register", authController.registerUser)
 router.post("/login", authController.loginUser)
 
