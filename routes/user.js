6143f56a3a3171e3ce4293e524dabab88293aa09
Salami Azeez <salamitopeazeez@gmail.com>
2024-04-13 13:39:53 -0700

    Added swagger ui for update user api

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,26 @@ import userController from "../controller/userController.js"
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * /api/{id}:
+ *  put:
+ *   summary: Update user info
+ *   description: Allows new user to register info
+ *   parameters: 
+ *      - in: path
+ *        name: id
+ *        type: integer
+ *        required: true
+ *        description: User ID
+ *   tags:
+ *    - User Info
+ *   responses:
+ *      '200':
+ *          description: Information updated successfully
+ *      '400':
+ *          description: Error updating user 
+ */
 router.put("/:id", userController.updateUser)  
 router.delete("/:id", userController.deleteUser)  
 
