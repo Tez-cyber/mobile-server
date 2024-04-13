98cfb399cc36aa998d57f0666f1ecddb673f3573
Salami Azeez <salamitopeazeez@gmail.com>
2024-04-12 18:51:57 -0700

    added description to info object and moved servers into info object

--- index.js
+++ index.js
@@ -27,21 +27,21 @@ const startServer = async () => {
 
         //routes
         app.use("/api/auth", authRoute)
-        app.get("/" , (req, res) => {
-            res.send("Server is now Launched") 
-        })
+    
         const options = {
             definition: {
                 openai: "3.0.0",
                 info: {
                     title: "Mobile App Server",
-                    version: "1.0.0"
+                    version: "1.0.0",
+                    description: "Swagger documention setup",
+                    servers: [
+                        {
+                            url: `http://localhost:${PORT}`
+                        }
+                    ]
                 },
-                servers: [
-                    {
-                        url: `http://localhost:${PORT}`
-                    }
-                ]
+                
             }, 
             apis: ["./routes/*.js"]
         }
@@ -50,6 +50,21 @@ const startServer = async () => {
         app.use(
             "/api-docs", swaggerui.serve, swaggerui.setup(swaggerSpec)
         )
+        /**
+         * 
+         * @swagger
+         * /:
+         *  get:
+         *      summary: This api is used to check if get method is functioning
+         *      description: Check get method
+         *      responses: 
+         *          200:
+         *              descripition: To test get method 
+         * 
+         */
+        app.get("/" , (req, res) => {
+            res.send("Server is now Launched") 
+        })
         // Starting app
         app.listen(PORT, () => {
             console.log(`Server started on port: ${PORT}`)
