18b8385fead70b553282b447adf3054c27f6b281
Salami Azeez <salamitopeazeez@gmail.com>
2024-04-12 18:39:28 -0700

    Implement swaggerUi in the root index file

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import serverless from "serverless-http"
 import { config } from "./config/config.js";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js"
+import swaggerjsdoc from "swagger-jsdoc"
+import swaggerui from "swagger-ui-express"
+
 
 
 const PORT = config.server.port
@@ -27,7 +30,26 @@ const startServer = async () => {
         app.get("/" , (req, res) => {
             res.send("Server is now Launched") 
         })
+        const options = {
+            definition: {
+                openai: "3.0.0",
+                info: {
+                    title: "Mobile App Server",
+                    version: "1.0.0"
+                },
+                servers: [
+                    {
+                        url: `http://localhost:${PORT}`
+                    }
+                ]
+            }, 
+            apis: ["./routes/*.js"]
+        }
 
+        const swaggerSpec = swaggerjsdoc(options)
+        app.use(
+            "/api-docs", swaggerui.serve, swaggerui.setup(swaggerSpec)
+        )
         // Starting app
         app.listen(PORT, () => {
             console.log(`Server started on port: ${PORT}`)
