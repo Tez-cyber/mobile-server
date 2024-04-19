617183e9f1b5eb47ed32432a2a5e8792f485efb1
Salami Azeez <salamitopeazeez@gmail.com>
2024-04-19 16:30:00 -0700

    Created intializePayment and callback function in paystack

---
+++ utils/payments.js
@@ -0,0 +1,21 @@
+const paystack = (req) => {
+    const MySecretKey = process.env.PAYSTACK_SECRET_KEY
+
+    const initializePayment = (form, mycallback) => {
+        const options = {
+            url: 'https://api.paystack.co/transaction/initialize',
+            headers: {
+                authorization: MySecretKey,
+                'content-type': 'application/json',
+                'cache-control': 'no-cache'
+            }, form
+        }
+
+        const callback = (err, res, body) => {
+            return mycallback(err, body)
+        }
+        req.post(options, callback)
+    }
+
+    
+}
