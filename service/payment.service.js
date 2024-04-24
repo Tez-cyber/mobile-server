d24f1d16a59a01a4da1f27ca82c33c07b381f137
Salami Azeez <salamitopeazeez@gmail.com>
2024-04-24 13:56:51 +0100

    Created createPayment function

--- service/payment.service.js
+++ service/payment.service.js
@@ -1,6 +1,6 @@
 import request from "request"
 import Payment from "../model/payment.js"
-import lodash from "lodash"
+import lodash, { reject } from "lodash"
 
 import { initializePayment, verifyPayment } from "../utils/payments.js";
 
@@ -29,5 +29,32 @@ class PaymentService {
         })
     }
 
-    
+    createPayment(req) {
+        const ref = req.reference;
+        if(ref == null) {
+            return  reject({ code: 400 , msg: 'No reference passed in query!' })
+        }
+        return new Promise(async (resolve, reject) => {
+            try {
+                verifyPayment(ref, (error, body) => {
+                    if(error) {
+                        reject(error.message)
+                    }
+                    const response = JSON.parse(body)
+
+                    const { reference, amount, status } = response.data
+                    const {email} = response.data.customer
+                    const full_name = response.data.metadata.full_name
+                    const newPayment = {reference, amount, email, full_name, status}
+                    const payment = Payment.create(newPayment)
+
+                    return resolve(payment)  
+                })
+            }catch(error) {
+                error.source = 'Create Payment Service';
+                return reject(error)
+            }
+        })
+    }
+
 }
