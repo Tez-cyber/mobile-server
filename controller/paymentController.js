85ae875fe5b19bfd6e68478475501da4fad97d5c
Salami Azeez <salamitopeazeez@gmail.com>
2024-04-24 14:09:36 +0100

    created getPayment function

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -18,3 +18,11 @@ export const createPayment = async (req, res) => {
         res.status(500).json({status: "Failed", message: error.message})
     }
 }
+export const getPayment = async (req, res) => {
+    try {
+        const response = await paymentInstance.paymentReciept(req.body)
+        res.status(201).json({status: "Success", data: response})
+    }catch(error) {
+        res.status(500).json({status: "Failed", message: error.message})
+    }
+}
